0586ebeba7f19c3f85cdc0ebfc2553bab98596c5
Frank Denis <github@pureftpd.org>
2017-01-27 10:48:32 +0100

    Linux uses a different value than BSD for IP_TOS

--- src/dscp.rs
+++ src/dscp.rs
@@ -1,19 +1,27 @@
+use libc::c_int;
 use std::io::Error;
-use std::mem::size_of_val;
 use std::os::unix::io::RawFd;
-use libc::{c_int, c_void, setsockopt, socklen_t, IPPROTO_IP};
 use Priority;
 
-const IP_TOS: c_int = 3;
-const IPTOS_DSCP_AF31: c_int = 0x68;
+#[cfg(target_os = "linux")]
+const IP_TOS: c_int = 1;
 
-#[cfg(not(unix))]
+#[cfg(any(target_os = "openbsd", target_os = "freebsd", target_os = "dragonfly", target_os = "bitrig", target_os = "macos"))]
+const IP_TOS: c_int = 3;
+
+#[cfg(not(any(target_os = "linux", target_os = "openbsd", target_os = "freebsd", target_os = "dragonfly", target_os = "bitrig", target_os = "macos")))]
+#[allow(unused_variables)]
 pub fn set_dscp_for_priority(fd: RawFd, prio: Priority) -> Result<(), Error> {
     Ok(())
 }
 
-#[cfg(unix)]
+#[cfg(any(target_os = "linux", target_os = "openbsd", target_os = "freebsd", target_os = "dragonfly", target_os = "bitrig", target_os = "macos"))]
 pub fn set_dscp_for_priority(fd: RawFd, prio: Priority) -> Result<(), Error> {
+    use std::mem::size_of_val;
+    use libc::{c_void, setsockopt, socklen_t, IPPROTO_IP};
+
+    const IPTOS_DSCP_AF31: c_int = 0x68;
+
     let tos: c_int = match prio {
         Priority::Interactive => IPTOS_DSCP_AF31,
         Priority::Default => 0x0,
